e3f767bb54511bb7814de730d06f625d59779691
John Chan <john@innovation.nj.gov>
2026-04-09 11:26:08 -0400

    add header

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,4 +1,5 @@
-import { Label, TextInput, TextInputMask } from "@trussworks/react-uswds";
+import { Label, TextInputMask } from "@trussworks/react-uswds";
+import njStateSeal from "@newjersey/njwds/dist/img/nj_state_seal.png";
 
 /**
  * Renders the full landing page for the NJ Property Tax Relief status checker.
@@ -8,6 +9,52 @@ import { Label, TextInput, TextInputMask } from "@trussworks/react-uswds";
  * before checking their individual application status.
  */
 export const LandingPage = () => (
+  <>
+      <a className="usa-skipnav" href="#main-content">
+        Skip to main content
+      </a>
+      <header className="nj-banner" aria-label="Official government website">
+        <div className="nj-banner__header">
+          <div className="grid-container">
+            <div className="nj-banner__inner">
+              <div>
+                <img src={njStateSeal} className="nj-banner__header-seal" alt="NJ flag" />
+              </div>
+              <div className="grid-col-fill">
+                <a href="https://nj.gov" target="_blank" rel="noopener">
+                  <span className="usa-sr-only">opens in a new tab.</span>
+                  Official Site of the State of New Jersey
+                </a>
+              </div>
+              <div className="grid-col-auto">
+                <div className="text-white">
+                  <ul>
+                    <li>
+                      <a href="https://nj.gov/governor/" target="_blank" rel="noopener">
+                        Governor Mikie Sherrill &bull; Lt. Governor Dr. Dale G. Caldwell
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://nj.gov/subscribe/" target="_blank" rel="noopener">
+                        <svg
+                          className="usa-icon nj-banner__mail-icon bottom-neg-2px margin-right-05"
+                          aria-hidden="true"
+                          focusable="false"
+                          role="img"
+                        >
+                          <use href={`/public/img/sprite.svg#mail`}></use>
+                        </svg>
+                        <span className="usa-sr-only">opens in a new tab.</span>
+                        Get Updates
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
   <main id="main-content">
     <section className="usa-section">
       <div className="grid-container">
@@ -27,4 +74,5 @@ export const LandingPage = () => (
       </div>
     </section>
   </main>
+  </>
 );
