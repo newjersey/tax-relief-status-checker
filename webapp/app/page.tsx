ff2dfb2598fb60f8b2a3b5279906d24dd9b0e4ef
John Chan <john@innovation.nj.gov>
2026-06-22 16:16:11 -0400

    removed unneeded declaration of sessionID

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Label, TextInputMask, Form, Button } from "@trussworks/react-uswds";
 import { useForm } from "react-hook-form";
@@ -9,7 +9,7 @@ import type { SubmitHandler } from "react-hook-form";
 import { LandingPageFaq, expandFaqAccordionItem } from "@/components/LandingPageFaq";
 import { maskSsn } from "@/app/utils/maskSsn";
 import { formatDate } from "@/app/utils/formatDate";
-import { logGAEvent, setSessionId } from "./utils/analytics";
+import { logGAEvent } from "./utils/analytics";
 
 /** Form data collected from the user. */
 interface UserData {
@@ -31,9 +31,6 @@ const returnToTop = () => {
 
 const LandingPage = () => {
   const router = useRouter();
-  useEffect(() => {
-    setSessionId();
-  }, []);
   const [apiError, setApiError] = useState<ReactNode | null>(null);
 
   const {
