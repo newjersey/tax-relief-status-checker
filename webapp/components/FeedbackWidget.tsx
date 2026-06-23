ef87a881514bd42c443a9816dcff7dce503bbd82
John Chan <john@innovation.nj.gov>
2026-06-23 15:38:51 -0400

    fix build by making feedback widget only render client-side

--- webapp/components/FeedbackWidget.tsx
+++ webapp/components/FeedbackWidget.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useRef, type ReactElement } from "react";
-import "@newjersey/feedback-widget/feedback-widget.min.js";
+import { type ReactElement } from "react";
+import dynamic from "next/dynamic";
 
 // NJ: Tells TypeScript that <feedback-widget> is a valid custom element.
 declare module "react" {
@@ -12,17 +12,19 @@ declare module "react" {
   }
 }
 
-export const FeedbackWidget = (): ReactElement => {
-  const widgetRef = useRef<HTMLElement>(null);
+// Allows us to render this dynamically to prevent server side rendering
+const FeedbackWidgetClientOnly = (): ReactElement => {
+  import("@newjersey/feedback-widget/feedback-widget.min.js");
 
   return (
     <>
       <section aria-label="Leave Feedback">
-        <feedback-widget
-          ref={widgetRef}
-          contact-link="https://www.nj.gov/treasury/taxation/contact.shtml?open=email"
-        ></feedback-widget>
+        <feedback-widget contact-link="https://www.nj.gov/treasury/taxation/contact.shtml?open=email"></feedback-widget>
       </section>
     </>
   );
 };
+
+export const FeedbackWidget = dynamic(() => Promise.resolve(FeedbackWidgetClientOnly), {
+  ssr: false,
+});
--- webapp/global.d.ts
+++ webapp/global.d.ts
@@ -2,3 +2,4 @@ declare module "*.css";
 declare module "*.png";
 declare module "*.svg";
 declare module "*.jpg";
+declare module "@newjersey/feedback-widget/feedback-widget.min.js";
