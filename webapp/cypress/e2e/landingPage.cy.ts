be6eaefb9908c430dff2b98e6caa8c97ed1b46a6
John Chan <john@innovation.nj.gov>
2026-06-25 10:48:34 -0400

    add first e2e test

--- webapp/cypress/e2e/landingPage.cy.ts
+++ webapp/cypress/e2e/landingPage.cy.ts
@@ -1,3 +1,19 @@
+const fillField = (fieldName: string, value: string): undefined => {
+  cy.get(`input[name="${fieldName}"]`).type(value);
+};
+
 it("should allow the user to visit the webpage", () => {
   cy.visit("/");
+  cy.window().its("scrollY").should("equal", 0); // The page view should be at the top
+});
+
+it("should display an error message when the user tries to submit empty fields", () => {
+  cy.visit("/");
+  cy.get(`button[type="submit"]`).click();
+  const ssnError = cy.get(`span[id="ssnErrorMessage"]`);
+  const zipError = cy.get(`span[id="zipCodeErrorMessage"]`);
+  ssnError.should("be.visible");
+  ssnError.should("contain.text", "This question is required");
+  zipError.should("be.visible");
+  zipError.should("contain.text", "This question is required");
 });
